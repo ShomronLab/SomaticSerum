a36c36a2d776725bebda65f32f556861ecd3fa4b
AlonWolfi <alonwolfy@gmail.com>
2022-05-17 12:36:39 +0300

    refactor

--- SomaticExtract/createEnv.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -ex
-SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-
-module load miniconda/miniconda-py38_4.9.2-environmentally
-conda env create -f ${SCRIPT_DIR}/refs/SomaticExtract_env.yml
